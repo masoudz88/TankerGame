0e21022f3d696a53000a2d5a25edcead8185fc44
masoudz88 <masoudz@mun.ca>
2020-10-27 09:57:24 -0230

    move the tank using arrow keys

--- src/game.js
+++ src/game.js
@@ -9,6 +9,16 @@ let image= new Image();
 image.src='images/tank_2.png'
 c.drawImage(image,100,400,150,150)
 
+let modifier=5;
+window.addEventListener('keydown', (Event) => {
+    const{style}=block;
+    switch (Event.key) {
+        case 'arrowUp':style.top= `${parseInt(style.top-modifier)}px`; break;
+        case 'arrowDown':style.top= `${parseInt(style.top-modifier)}px`; break;
+        case 'arrowLeft':style.left= `${parseInt(style.left-modifier)}px`; break;
+        case 'arrowRight':style.left= `${parseInt(style.left-modifier)}px`; break;
+    }
+});
 
 
 
