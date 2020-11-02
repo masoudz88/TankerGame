b0644d33ebb5d96f26ca6694b4469d61db6f3a7d
masoudz88 <masoudz@mun.ca>
2020-11-02 12:32:24 -0330

    adding and moving tank image using OOP

--- src/game.js
+++ src/game.js
@@ -1,24 +1,42 @@
 // call the canvas from index.html
-let canvas= document.querySelector("canvas");
-//set the dimensions of canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let c = canvas.getContext('2d');
+function Tank(x,y,xmove,ymove){
+    this.x=x
+    this.y=y
+    this.xmove= xmove
+    this.ymove=ymove
+    let canvas= document.querySelector("canvas");
+    //set the dimensions of canvas
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let c = canvas.getContext('2d');
+    // adding tank image on canvas
+    let image= new Image();
+    image.src='images/tank_2.png'
+    //c.drawImage(image,this.x,this.y,150,150)
 
-let image= new Image();
-image.src='images/tank_2.png'
-c.drawImage(image,100,400,150,150)
-
-let modifier=5;
-window.addEventListener('keydown', (Event) => {
-    const{style}=block;
-    switch (Event.key) {
-        case 'arrowUp':style.top= `${parseInt(style.top-modifier)}px`; break;
-        case 'arrowDown':style.top= `${parseInt(style.top-modifier)}px`; break;
-        case 'arrowLeft':style.left= `${parseInt(style.left-modifier)}px`; break;
-        case 'arrowRight':style.left= `${parseInt(style.left-modifier)}px`; break;
+    this.getX= function(){
+        return this.x
     }
-});
+    this.update= function(){
+        this.x=this.x+this.xmove;
+        this.y=this.y+this.ymove;
+        
+    };
+    this.draw= function(){        
+        this.update();
+        c.drawImage(image,this.x,this.y,150,150)
+    };
+    
+}
+let tank1=new Tank(200,400,200,100);
+tank1.draw()
+
+console.log(tank1.getX())
+
+
+
+
+
 
 
 
