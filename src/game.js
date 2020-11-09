72b9f762533a381c0d74b664bbd879aed3444e55
masoudz88 <masoudz@mun.ca>
2020-11-09 12:45:43 -0330

    move the tank

--- src/game.js
+++ src/game.js
@@ -1,51 +1,48 @@
 // call the canvas from index.html
-function Tank(x,y){
-    this.x=x
-    this.y=y
-    this.xmove= 10
-    this.ymove= 10
-    
-    let canvas= document.querySelector("canvas");
-    //set the dimensions of canvas
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    let c = canvas.getContext('2d');
-    // adding tank image on canvas
-    let image= new Image();
-    image.src='images/tank_2.png'
-    //c.drawImage(image,this.x,this.y,150,150)
-
-    this.getX= function(){
-        return this.x
+class Tank {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.c = document.getElementById("myCanvas");            
+        this.c.width = window.innerWidth;
+        this.c.height = window.innerHeight;        
+        this.ctx = this.c.getContext("2d");
+        this.img = document.getElementById("tank");
+        console.log("Tank")
     }
-    /*this.update= function(){
-        this.x=this.x+this.xmove;       
         
-    };*/
-    this.show= function(){         
-        c.drawImage(image,this.x,this.y,150,150)
-    };
-    //moving tank object with arrow keys
-    let interval;
-    document.addEventListener("keydown", Event =>{        
-            if (Event.key === ArrowRight) {
-                interval=setInterval(moveRight, this.xmove)
-            } //right arrow
-            else if (Event.key === ArrowLeft) {
-                interval=setInterval(moveLeft, this.xmove)
-            } //left arrow        
-        });  
-    document.addEventListener("keyup", Event =>{   
-        clearInterval(interval)
-    } );  
+        drawTank(){             
+            this.ctx.drawImage(this.img, this.x, this.y,150,150);            
+        }
+        movement(){
+            document.addEventListener("keydown", Event =>{        
+                
+                if (Event.key === "ArrowRight") {
+                    console.log("right");
+                    this.x+=10; 
+                    this.ctx.drawImage(this.img, this.x, this.y,150,150);    
+                    
+                    } //right arrow
+                else if (Event.key === "ArrowLeft") {
+                    console.log("left");
+                    this.x-=10;
+                    this.ctx.drawImage(this.img, this.x, this.y,150,150);
+                    } //left arrow        
+                });
+        }
+    }
+
+    const myTank= new Tank(200,400)
+    myTank.drawTank()    
+    myTank.movement()    
+    console.log(myTank)
     
-}
-    
+ 
+ 
+     
     
 
 
-let tank1=new Tank(200,400);
-tank1.show()
 
 
 
